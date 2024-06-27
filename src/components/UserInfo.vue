f8143b06e1b8d1b9b48a66c4fb6d86be4124ada7
Nikola Gigic <nik.gigic@gmail.com>
2024-06-27 12:32:14 +0200

    feat: notif bubble for user avatar

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -2,11 +2,16 @@
 
 <template>
   <main class="flex w-full justify-end p-5">
-    <button class="group relative h-12 w-12 overflow-hidden rounded-full">
+    <button class="group relative h-14 w-14 overflow-hidden rounded-full">
       <div
         class="absolute hidden h-full w-full bg-black/50 group-hover:block group-active:bg-black/30"
       />
       <img src="/user-image.png" />
+      <div
+        class="absolute right-2 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-dark-basics-accent-red p-1 text-xs font-bold text-white"
+      >
+        8
+      </div>
     </button>
   </main>
 </template>
